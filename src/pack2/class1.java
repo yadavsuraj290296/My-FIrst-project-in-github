cbc3f118480a2bf0d6bc0f3e5eaeed282ca2e12c
suraj <suraj@LAPTOP-62C0PDQP>
2022-08-07 13:39:50 +0530

    2nd project push on github

---
+++ src/pack2/class1.java
@@ -0,0 +1,11 @@
+package pack2;
+
+public class class1 {
+public static void main(String[] args) {
+	System.out.println("hi my name is suraj");
+	System.out.println("my surname is yadav ");
+	System.out.println("i was comleted my graduation in nagpur unvercity");
+	System.out.println("project us end");
+	
+}
+}
