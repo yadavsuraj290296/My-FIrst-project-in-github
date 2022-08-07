ed0f64fdb954913ed8658a31b241cc4734e3a3d2
suraj <suraj@LAPTOP-62C0PDQP>
2022-08-07 14:03:30 +0530

    new adition of the code

--- src/pack2/class1.java
+++ src/pack2/class1.java
@@ -6,6 +6,6 @@ public static void main(String[] args) {
 	System.out.println("my surname is yadav ");
 	System.out.println("i was comleted my graduation in nagpur unvercity");
 	System.out.println("project us end");
-	
+	System.out.println("my new addition of code");
 }
 }
